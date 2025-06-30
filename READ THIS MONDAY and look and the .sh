4e68eece60b3c1ccbe38386c5ece2cebacd9211c
Adam Shawhatti <69728493+AShawhatti@users.noreply.github.com>
2025-06-30 18:55:02 -0400

    Create READ THIS MONDAY and look and the .sh

---
+++ READ THIS MONDAY and look and the .sh
@@ -0,0 +1,8 @@
++------------+------------+-----------+
+| Customerid | First_Name | Last_Name |
++------------+------------+-----------+
+|          1 | John       | Smith     |
+|          2 | Bob        | Robert    |
+|          3 | Brother    | Haroon    |
+|          4 | Haroon     | Brother   |
++------------+------------+-----------+
